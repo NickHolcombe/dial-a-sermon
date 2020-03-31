a36958e630b72f544952049aca9a5cbbfcf0001f
Nick <nick.holcombe@gmail.com>
2020-03-31 20:09:55 +0100

    Create twilio-function.js

---
+++ twilio-function.js
@@ -0,0 +1,43 @@
+exports.handler = function(context, event, callback) {
+    let twiml = new Twilio.twiml.VoiceResponse();
+    var fetch = require('node-fetch');
+    
+    // Define the list of sermons and fetch the data
+    const url = 'https://raw.githubusercontent.com/NickHolcombe/dial-a-sermon/master/db.json';
+    fetch(url)
+    .then(response => response.json())
+    .then(function(data) {
+        // Validate we have some sermon data
+        const sermons = data.sermons;
+        if (data === undefined || sermons === undefined || sermons.length === 0) {
+            twiml.say({voice: 'alice', language: 'en-gb'}, 'Sorry an error occurred fetching the sermons');
+    	    callback(null, twiml);
+    	    twiml.hangup();
+        }
+        
+        // Produce a menu based, on the sermons found, if no phone key(digit) was pressed...
+        if (event.Digits === undefined || event.Digits > sermons.length) {
+            const sermonMenu = sermons.map(sermon => 'Press '+sermon.id+' for '+sermon.title).join(",");
+            twiml.gather({numDigits: 1})
+            .say({voice: 'alice', language: 'en-gb'}, 'Welcome to All Saints Crow burra, '+sermonMenu)
+        } else {
+            // ... otherwise, get the right sermon based on the key pressed.
+            const sermonIndex = event.Digits - 1;
+            const sermon = sermons[sermonIndex];
+            
+            if (sermon === undefined) {
+                twiml.say({voice: 'alice', language: 'en-gb'}, 'Sorry an error occurred fetching the sermons');
+    	        callback(null, twiml);
+    	        twiml.hangup();
+            }
+            
+          	twiml.say({voice: 'alice', language: 'en-gb'}, 'Please wait whilst we fetch the sermon.');
+    	    twiml.play(sermon.url);
+    	    twiml.hangup();
+        }
+        callback(null, twiml);
+    })
+    .catch(function(error) {
+        callback(error)
+    })
+};
